dc239103210dbaae6c827b875616751173cf71fb
maneshkakarla <maneshkakarla@gmail.com>
2026-04-15 23:31:35 +0530

    added  manesh.py

---
+++ manesh.py
@@ -0,0 +1,20 @@
+name = "Manesh"
+age = 20
+height = 1.70
+is_student = True
+
+print(name, age, height, is_student)
+
+
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_square = age ** 2
+
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Remainder when age is divided by 7:", remainder)
+print("Age squared:", age_square)
